202f96d33b43fb4a270a1cf6d511dfe1e833f70b
DanielPickens <72703981+DanielPickens@users.noreply.github.com>
2022-03-06 20:20:34 -0700

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -27,7 +27,5 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    
   ],
 }
