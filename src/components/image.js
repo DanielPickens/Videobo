1a53d1c85af0690f4d02a20558d54fd736df0991
DanielPickens <72703981+DanielPickens@users.noreply.github.com>
2022-10-30 22:40:27 -0600

    Update image.js

--- src/components/image.js
+++ src/components/image.js
@@ -20,29 +20,6 @@ const Image = () => {
   return <Img fluid={data.placeholderImage.childImageSharp.fluid} />
 }
 
-//improve the image component
-function Image({ src, alt, ...props }) {
-  
-
-  const data = useStaticQuery(graphql`
-
-    query {
-      allFile {
-        edges {
-          node {
-            relativePath
-            name
-            childImageSharp {
-              fluid(maxWidth: 300) {
-                ...GatsbyImageSharpFluid
-              }
-            }
-          }
-        }
-      }
-    }
-  `)
-  }
 
 export default Image
 
